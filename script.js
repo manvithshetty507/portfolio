1b0d285520f06abb1fb8933ae005607966fbb8c6
manvithshetty507 <manvithshetty18@gmail.com>
2024-03-24 18:03:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const menu = document.querySelector('.menu__icon');
+const close = document.querySelector('.close__icon');
+const navbarList = document.querySelector('.navbar > ul');
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    menu.addEventListener('click', function () {
+        menu.classList.toggle('active');
+        close.classList.toggle('active');
+        navbarList.style.display = 'block'
+        navbarList.classList.toggle('active__list');
+    });
+
+    close.addEventListener('click', function () {
+        menu.classList.toggle('active');
+        close.classList.toggle('active');
+        navbarList.classList.toggle('active__list');
+        navbarList.style.display = 'none'
+    });
+});
+
+// Function to handle window resize
+window.addEventListener('resize', function () {
+    // If the window width exceeds 725px (desktop view), ensure the navbar list is shown
+    if (window.innerWidth > 725) {
+        navbarList.style.display = 'block';
+    }
+});
